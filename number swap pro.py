985378074fad5dc37caced732526657646ac97bb
AamirCode2 <aamirlovesart@gmail.com>
2025-08-02 22:12:46 +1000

    Add files via upload

---
+++ number swap pro.py
@@ -0,0 +1,15 @@
+#The three numbers from Tiya's mathematical problem are
+
+num1 = 5.6
+num2 = 8
+num3 = 3.7
+
+#She accidentally wrote the programming wrong and to make the mathematical problem correct abain we need to swap the numbers
+
+num1,num2,num3 = num3,num1,num2
+
+print(num1)
+print(num2)
+print(num3)
+
+#We have succesfully swapped the numbers
